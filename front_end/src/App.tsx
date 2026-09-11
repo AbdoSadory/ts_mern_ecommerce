4453100d4862886820ead58f587e905ef91c379d
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2026-09-11 23:02:41 +0300

    adding bootstrap and create ROW and COL are done all

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,34 +1,52 @@
-import { useState } from "react"
-import heroImg from "./assets/hero.png"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "./assets/vite.svg"
-import "./App.css"
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap"
 import { sampleProducts } from "./data"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <header>TS Amazon</header>
-      <main>
-        {sampleProducts.map((product) => (
-          <div key={product.slug}>
-            <h2>{product.name}</h2>
-            <img
-              src={product.image}
-              alt={product.name}
-              className="product-image"
-            />
-            <p>{product.description}</p>
-            <p>Price: ${product.price}</p>
-            <p>
-              Rating: {product.rating} ({product.numReviews} reviews)
-            </p>
-          </div>
-        ))}
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" className="px-3">
+          <Container>
+            <Navbar.Brand>TS Amazon</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main className="container my-3">
+        <Row>
+          {sampleProducts.map((product) => (
+            <Col
+              key={product.slug}
+              className="product-card d-flex flex-column vh-100"
+              sm={6}
+              md={4}
+              lg={3}
+            >
+              <h2>{product.name}</h2>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <p>{product.description}</p>
+              <p>Price: ${product.price}</p>
+              <p>
+                Rating: {product.rating} ({product.numReviews} reviews)
+              </p>
+            </Col>
+          ))}
+        </Row>
       </main>
-      <footer>All rights reserved</footer>
+      <footer className="bg-dark text-white text-center py-3 mt-3">
+        <div>All rights reserved</div>
+      </footer>
     </div>
   )
 }
